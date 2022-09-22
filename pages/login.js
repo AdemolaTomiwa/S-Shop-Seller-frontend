901de7d10c438ea3c5628f164518166cb136d184
thetommedia@gmail.com <thetommedia@gmail.com>
2022-09-22 16:02:44 +0100

    Added back btn

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,10 @@
 import LoginForm from '../components/LoginpageComponents/LoginForm';
+import BackBtn from '../components/BackBtn';
 
 export default function Home() {
    return (
       <div className="login-page">
+         <BackBtn to="/" />
          <LoginForm />
       </div>
    );
