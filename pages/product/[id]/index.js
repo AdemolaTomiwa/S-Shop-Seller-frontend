d4733facd92cfe33c500983421ff1ac111ee7302
thetommedia@gmail.com <thetommedia@gmail.com>
2022-09-22 16:26:14 +0100

    Made

--- pages/product/[id]/index.js
+++ pages/product/[id]/index.js
@@ -1,11 +1,15 @@
 import ProductWrapper from '../../../components/ProductPageComponents/ProductWrapper';
 import BackBtn from '../../../components/BackBtn';
+import SellerNotification from '../../../components/SellerNotification';
 
 const product = () => {
    return (
       <div className="product-page page">
+        <SellerNotification />
          <BackBtn to="/allproducts" />
          <ProductWrapper />
+        <SellerNotification />
+         
       </div>
    );
 };
