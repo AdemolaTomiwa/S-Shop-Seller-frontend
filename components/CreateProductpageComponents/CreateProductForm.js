7b1b04c63c8b444223d6cff0077cb7011848ee7e
thetommedia@gmail.com <thetommedia@gmail.com>
2022-09-25 00:03:08 +0100

    Initial commit

--- components/CreateProductpageComponents/CreateProductForm.js
+++ components/CreateProductpageComponents/CreateProductForm.js
@@ -128,20 +128,16 @@ const CreateProductForm = () => {
                   >
                      <option value="all">All</option>
                      <option value="bags">bags</option>
-                     <option value="cap">cap</option>
+                     <option value="caps">caps</option>
                      <option value="clothes">clothes</option>
-                     <option value="frangrance">frangrance</option>
+                     <option value="frangrances">frangrances</option>
                      <option value="glasses">glasses</option>
-                     <option value="hair accessory">jewerly</option>
-                     <option value="smart phone">
-                        smart phone and accessory
-                     </option>
-                     <option value="skin and hair care">
-                        skin and hair care
-                     </option>
+                     <option value="jewerly">jewerly</option>
+                     <option value="phones">smart phone</option>
+                     <option value="skins">skin and hair care</option>
                      <option value="shoes">shoes</option>
                      <option value="watches">watches</option>
-                     <option value="wallpaper">wallpaper</option>
+                     <option value="wallpapers">wallpapers</option>
                      <option value="all">others</option>
                   </select>
                </div>
