397a6ec98e9d31414961cc436d61b1c53c446fa4
thetommedia@gmail.com <thetommedia@gmail.com>
2022-12-31 18:35:28 +0100

    Changed from Heroku to Render

--- config/server.js
+++ config/server.js
@@ -1,2 +1,3 @@
 // export const server = 'http://localhost:8080';
-export const server = 'https://s-s-h-o-p-s-e-l-l-e-r.herokuapp.com';
+// export const server = 'https://s-s-h-o-p-s-e-l-l-e-r.herokuapp.com';
+export const server = 'https://sellsshop.onrender.com';
